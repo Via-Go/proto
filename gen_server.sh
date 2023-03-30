58137028ebbac86fda15f385737f7ffc01b01eab
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-31 00:36:10 +0200

    add protoc download to server gen script

--- gen_server.sh
+++ gen_server.sh
@@ -6,6 +6,11 @@ readonly go_gen_path=$2
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
+unzip protoc-3.15.8-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+
 PATH="$PATH:$(go env GOPATH)/bin"
 export PATH
 
