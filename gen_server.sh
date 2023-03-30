4f39fc8457f9aed1f4693789febeb8b204f660be
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-31 01:01:15 +0200

    log protoc version inside server gen script

--- gen_server.sh
+++ gen_server.sh
@@ -7,7 +7,7 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 
 apt install -y protobuf-compiler
-echo protoc --version
+protoc --version
 
 PATH="$PATH:$(go env GOPATH)/bin"
 export PATH
